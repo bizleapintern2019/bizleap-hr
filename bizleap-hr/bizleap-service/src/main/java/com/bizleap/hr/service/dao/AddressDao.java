fbaaa53992d89f80fefca6655bff7e509f81c6d4
Soe Min Thein <BizLeap2019@gmail.com>
2019-06-25 16:21:25 +0630

    com

---
+++ bizleap-hr/bizleap-service/src/main/java/com/bizleap/hr/service/dao/AddressDao.java
@@ -0,0 +1,9 @@
+package com.bizleap.hr.service.dao;
+
+import com.bizleap.commons.domain.entity.Address;
+import com.bizleap.commons.domain.entity.Department;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+
+public interface AddressDao extends AbstractDao<Address,String>{
+	public void save(Address address)throws ServiceUnavailableException;
+}
---
+++ bizleap-hr/bizleap-service/src/main/java/com/bizleap/hr/service/dao/impl/AddressDaoImpl.java
@@ -0,0 +1,19 @@
+package com.bizleap.hr.service.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.bizleap.commons.domain.entity.Address;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+import com.bizleap.hr.service.dao.AddressDao;
+
+@Repository
+public class AddressDaoImpl extends AbstractDaoImpl<Address,String> implements AddressDao{
+	 protected AddressDaoImpl() {
+	        super(Address.class);
+	    }
+	
+	 	@Override
+	    public void save(Address address) throws ServiceUnavailableException {
+	        saveOrUpdate(address);
+	    }
+}
