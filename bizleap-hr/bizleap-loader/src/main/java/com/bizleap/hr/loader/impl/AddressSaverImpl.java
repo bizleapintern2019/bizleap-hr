19ca526617f0af0471901ff3811e7355f3307850
Kay Zin Han <bizleapintern2019@gmail.com>
2019-06-25 16:56:38 +0630

    saver

---
+++ bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/impl/AddressSaverImpl.java
@@ -0,0 +1,40 @@
+package com.bizleap.hr.loader.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bizleap.commons.domain.entity.Address;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+import com.bizleap.commons.domain.utils.Printer;
+import com.bizleap.hr.loader.AddressSaver;
+import com.bizleap.service.AddressService;
+
+public class AddressSaverImpl implements AddressSaver{
+	
+	private static Logger logger = Logger.getLogger(DepartmentSaverImpl.class);
+    private static Printer printer = new Printer(logger);
+
+    @Autowired
+    AddressService addressService;
+
+    List<Address> addressList;
+
+    public void savePass1() throws ServiceUnavailableException, IOException {
+        printer.line("Saving Address: "+ getAddressList().size());
+        for(Address address:getAddressList()) {
+        	addressService.saveAddress(address);
+        }
+        printer.line("Saving Completed");
+    }
+
+    public void setAddressList(List<Address> addressList) {
+        this.addressList=addressList;
+    }
+
+    public List<Address> getAddressList() {
+        return this.addressList;
+    }
+}
---
+++ bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/impl/DepartmentSaverImpl.java
@@ -0,0 +1,40 @@
+package com.bizleap.hr.loader.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bizleap.commons.domain.entity.Department;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+import com.bizleap.commons.domain.utils.Printer;
+import com.bizleap.hr.loader.DepartmentSaver;
+import com.bizleap.service.DepartmentService;
+
+public class DepartmentSaverImpl implements DepartmentSaver{
+	
+	private static Logger logger = Logger.getLogger(DepartmentSaverImpl.class);
+    private static Printer printer = new Printer(logger);
+
+    @Autowired
+    DepartmentService departmentService;
+
+    List<Department> departmentList;
+
+    public void savePass1() throws ServiceUnavailableException, IOException {
+        printer.line("Saving Department: "+ getDepartmentList().size());
+        for(Department department:getDepartmentList()) {
+        	departmentService.saveDepartment(department);
+        }
+        printer.line("Saving Completed");
+    }
+
+    public void setDepartmentList( List<Department> departmentList) {
+        this.departmentList=departmentList;
+    }
+
+    public List<Department> getDepartmentList() {
+        return this.departmentList;
+    }
+}
