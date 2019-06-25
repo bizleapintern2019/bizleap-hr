dd01bd651bbea74dccbb4af3bb3b0ad75c09ec9f
Kay Zin Han <bizleapintern2019@gmail.com>
2019-06-25 16:25:35 +0630

    modified address conflicts

--- bizleap-commons/bizleap-domain/src/main/java/com/bizleap/commons/domain/entity/Address.java
+++ bizleap-commons/bizleap-domain/src/main/java/com/bizleap/commons/domain/entity/Address.java
@@ -1,6 +1,5 @@
 package com.bizleap.commons.domain.entity;
 
-<<<<<<< HEAD
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -96,8 +95,4 @@ public class Address extends AbstractEntity{
 		 		.append("State :"+getState())
 		 		.append("Country :"+getCountry());
 	}
-=======
-public class Address {
-
->>>>>>> 648de4acf9293affdde9b06313eafb329ab15316
 }
