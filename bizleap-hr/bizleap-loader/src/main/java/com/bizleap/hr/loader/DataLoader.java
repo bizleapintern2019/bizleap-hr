45534b8c9a8175634361a07fea11e5cf31362bad
KaungPyaeSoneTun <BizLeap2019@gmail.com>
2019-06-25 15:52:18 +0630

    loader push

--- bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/DataLoader.java
+++ bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/DataLoader.java
@@ -3,13 +3,22 @@ package com.bizleap.hr.loader;
 import java.util.List;
 import java.util.Map;
 
+import com.bizleap.commons.domain.entity.Address;
 import com.bizleap.commons.domain.entity.Company;
+import com.bizleap.commons.domain.entity.Department;
 import com.bizleap.commons.domain.entity.Employee;
 import com.bizleap.commons.domain.entity.ErrorCollection;
+import com.bizleap.commons.domain.entity.Job;
+import com.bizleap.commons.domain.entity.Location;
+import com.bizleap.commons.domain.entity.Position;
 
 public interface DataLoader {
 	public List<Employee> loadEmployee() throws Exception;
-	public List<Company> loadCompany() throws Exception;
+	public List<Department> loadDepartment() throws Exception;
+	public List<Location> loadLocation() throws Exception;
+	public List<Job> loadJob() throws Exception;
+	public List<Position> loadPosition() throws Exception;
+	public List<Address> loadAddress() throws Exception;
 	public int getIndex();
 	public void setIndex(int index);
 }
--- bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/impl/DataLoaderImpl.java
+++ bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/impl/DataLoaderImpl.java
@@ -7,9 +7,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bizleap.commons.domain.entity.Address;
 import com.bizleap.commons.domain.entity.Company;
+import com.bizleap.commons.domain.entity.Department;
 import com.bizleap.commons.domain.entity.Employee;
 import com.bizleap.commons.domain.entity.ErrorCollection;
+import com.bizleap.commons.domain.entity.Job;
+import com.bizleap.commons.domain.entity.Location;
+import com.bizleap.commons.domain.entity.Position;
 import com.bizleap.hr.loader.DataLoader;
 import com.bizleap.hr.loader.DataManager;
 import com.bizleap.hr.loader.ErrorCollector;
@@ -61,11 +66,12 @@ public class DataLoaderImpl implements DataLoader {
 		return employeeList;
 	}
 
-	public List<Company> loadCompany() throws Exception {
+	
+	public List<Location> loadLocation() throws Exception {
 		fileLoader.start("D://Com.txt");
 		String dataLine="";
-		List<Company> companyList= new ArrayList<Company>();
-		Company company=null;
+		List<Location> locationList= new ArrayList<Location>();
+		Location location=null;
 		while(fileLoader.hasMore()) {
 			try {
 				dataLine=fileLoader.getLine();
@@ -73,17 +79,121 @@ public class DataLoaderImpl implements DataLoader {
 					if(fileLoader.hasMore())
 					dataLine=fileLoader.getLine();
 				}
-				company = Company.parseCompany(dataLine);
-				if(company != null){
-					companyList.add(company);
+				location = Location.parseLocation(dataLine);
+				if(location != null){
+					locationList.add(location);
 				}
 			}catch (Exception e) {
-				errorCollector.handleLoadingError(index,fileLoader.getLineNumber(),"Company file loading.",dataLine);
+				errorCollector.handleLoadingError(index,fileLoader.getLineNumber(),"Location file loading.",dataLine);
 				index++;
 				
 			}
 		}
 		fileLoader.finish();
-		return companyList;
+		return locationList;
+	}
+	
+	public List<Department> loadDepartment() throws Exception {
+		fileLoader.start("D://Com.txt");
+		String dataLine="";
+		List<Department> departmentList= new ArrayList<Department>();
+		Department department=null;
+		while(fileLoader.hasMore()) {
+			try {
+				dataLine=fileLoader.getLine();
+				if(dataLine.startsWith("#")){
+					if(fileLoader.hasMore())
+					dataLine=fileLoader.getLine();
+				}
+				department = Department.parseDepartment(dataLine);
+				if(department != null){
+					departmentList.add(department);
+				}
+			}catch (Exception e) {
+				errorCollector.handleLoadingError(index,fileLoader.getLineNumber(),"Department file loading.",dataLine);
+				index++;
+				
+			}
+		}
+		fileLoader.finish();
+		return departmentList;
+	}
+	
+	public List<Job> loadJob() throws Exception {
+		fileLoader.start("D://Com.txt");
+		String dataLine="";
+		List<Job> jobList= new ArrayList<Job>();
+		Job job=null;
+		while(fileLoader.hasMore()) {
+			try {
+				dataLine=fileLoader.getLine();
+				if(dataLine.startsWith("#")){
+					if(fileLoader.hasMore())
+					dataLine=fileLoader.getLine();
+				}
+				job = Job.parseJob(dataLine);
+				if(job != null){
+					jobList.add(job);
+				}
+			}catch (Exception e) {
+				errorCollector.handleLoadingError(index,fileLoader.getLineNumber(),"Job file loading.",dataLine);
+				index++;
+				
+			}
+		}
+		fileLoader.finish();
+		return jobList;
+	}
+	
+	public List<Position> loadPosition() throws Exception {
+		fileLoader.start("D://Com.txt");
+		String dataLine="";
+		List<Position> positionList= new ArrayList<Position>();
+		Position position=null;
+		while(fileLoader.hasMore()) {
+			try {
+				dataLine=fileLoader.getLine();
+				if(dataLine.startsWith("#")){
+					if(fileLoader.hasMore())
+					dataLine=fileLoader.getLine();
+				}
+				position = Position.parsePosition(dataLine);
+				if(position != null){
+					positionList.add(position);
+				}
+			}catch (Exception e) {
+				errorCollector.handleLoadingError(index,fileLoader.getLineNumber(),"Position file loading.",dataLine);
+				index++;
+				
+			}
+		}
+		fileLoader.finish();
+		return positionList;
+	}
+	
+	public List<Address> loadAddress() throws Exception {
+		fileLoader.start("D://Com.txt");
+		String dataLine="";
+		List<Address> addressList= new ArrayList<Address>();
+		Address address=null;
+		while(fileLoader.hasMore()) {
+			try {
+				dataLine=fileLoader.getLine();
+				if(dataLine.startsWith("#")){
+					if(fileLoader.hasMore())
+					dataLine=fileLoader.getLine();
+				}
+				address = Address.parseAddress(dataLine);
+				if(address != null){
+					addressList.add(address);
+				}
+			}catch (Exception e) {
+				errorCollector.handleLoadingError(index,fileLoader.getLineNumber(),"Address file loading.",dataLine);
+				index++;
+				
+			}
+		}
+		fileLoader.finish();
+		return addressList;
 	}
 }
