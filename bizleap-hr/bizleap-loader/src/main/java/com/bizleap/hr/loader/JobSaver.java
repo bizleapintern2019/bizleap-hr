30381fae990afe053211a5cc45a15df72be26d0a
nyanlinhtet <bizleapintern2019@gmail.com>
2019-06-25 17:03:52 +0630

    EmployeeJobSaver

---
+++ bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/JobSaver.java
@@ -0,0 +1,12 @@
+package com.bizleap.hr.loader;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.bizleap.commons.domain.entity.Job;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+
+public interface JobSaver {
+	public void savePass1() throws ServiceUnavailableException, IOException;
+	public void setJobList(List<Job> jobList);
+}
