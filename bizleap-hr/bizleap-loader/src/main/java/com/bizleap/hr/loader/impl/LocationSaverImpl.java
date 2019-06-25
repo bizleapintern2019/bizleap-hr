ef4de6eb7a137e4c139d6ad42517e1b5368bce19
Soe Min Thein <BizLeap2019@gmail.com>
2019-06-25 16:53:11 +0630

    Savers Commit

--- bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/impl/LocationSaverImpl.java
+++ bizleap-hr/bizleap-loader/src/main/java/com/bizleap/hr/loader/impl/LocationSaverImpl.java
@@ -1,10 +1,14 @@
 package com.bizleap.hr.loader.impl;
 
 import com.bizleap.commons.domain.entity.Company;
+import com.bizleap.commons.domain.entity.Location;
 import com.bizleap.commons.domain.exception.ServiceUnavailableException;
 import com.bizleap.commons.domain.utils.Printer;
 import com.bizleap.hr.loader.CompanySaver;
+import com.bizleap.hr.loader.LocationSaver;
 import com.bizleap.service.CompanyService;
+import com.bizleap.service.LocationService;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,28 +17,28 @@ import java.io.IOException;
 import java.util.List;
 
 @Service
-public class CompanySaverImpl implements CompanySaver {
-    private static Logger logger = Logger.getLogger(CompanySaverImpl.class);
+public class LocationSaverImpl implements LocationSaver {
+    private static Logger logger = Logger.getLogger(LocationSaverImpl.class);
     private static Printer printer = new Printer(logger);
 
     @Autowired
-    CompanyService companyService;
+    LocationService locationService;
 
-    List<Company> companyList;
+    List<Location> locationList;
 
     public void savePass1() throws ServiceUnavailableException, IOException {
-        printer.line("Saving Company: "+ getCompanyList().size());
-        for(Company company:getCompanyList()) {
-            companyService.saveCompany(company);
+        printer.line("Saving Location: "+ getLocationList().size());
+        for(Location location:getLocationList()) {
+            locationService.saveLocation(location);
         }
         printer.line("Saving Completed");
     }
 
-    public void setCompanyList(List<Company> companyList) {
-        this.companyList=companyList;
+    public void setLocationList(List<Location> locationList) {
+        this.locationList=locationList;
     }
 
-    public List<Company> getCompanyList() {
-        return this.companyList;
+    public List<Location> getLocationList() {
+        return this.locationList;
     }
 }
