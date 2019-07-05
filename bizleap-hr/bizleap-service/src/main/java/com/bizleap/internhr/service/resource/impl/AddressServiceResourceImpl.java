985e55538a597ed9ef69bc3669cb52816005fc9c
shinewanna <bizleapintern2019@gmail.com>
2019-07-05 16:34:21 +0630

    Address Service resource

---
+++ bizleap-hr/bizleap-service/src/main/java/com/bizleap/internhr/service/resource/impl/AddressServiceResourceImpl.java
@@ -0,0 +1,81 @@
+package com.bizleap.internhr.service.resource.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bizleap.commons.domain.entity.Address;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+import com.bizleap.internhr.service.resource.AddressServiceResource;
+import com.bizleap.service.AddressService;
+
+@RestController
+@RequestMapping(value= {"/address"})
+public class AddressServiceResourceImpl implements AddressServiceResource {
+
+	@Autowired
+	AddressService addressService;
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/new")
+	public @ResponseBody boolean createAddress(HttpServletRequest request,@RequestBody Address address) {
+			try {
+				addressService.saveAddress(address);
+			} catch (IOException e) {
+				return false;
+			} catch (ServiceUnavailableException e) {
+				return false;
+			}
+			return true;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/list")
+	public @ResponseBody List<Address> getAllAddress(HttpServletRequest request) throws ServiceUnavailableException {
+		return addressService.getAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/{boId}")
+	public @ResponseBody List<Address> findByAddressBoId(HttpServletRequest request,
+		   @RequestParam(value = "boId") String boId) throws ServiceUnavailableException {
+		return addressService.findByBoId(boId);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/city")
+	public @ResponseBody List<Address> findByCity(HttpServletRequest request,
+		   @RequestParam(value = "city") String city) throws ServiceUnavailableException {
+		return addressService.findByCity(city);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/contactAddress")
+	public @ResponseBody List<Address> findByContactAddress(HttpServletRequest request,
+		   @RequestParam(value = "contactAddress") String contactAddress) throws ServiceUnavailableException {
+		return addressService.findByContactAddress(contactAddress);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/permanentAddress")
+	public @ResponseBody List<Address> findByPermanentAddress(HttpServletRequest request,
+		   @RequestParam(value = "permanentAddress") String permanentAddress) throws ServiceUnavailableException {
+		return addressService.findByPermanentAddress(permanentAddress);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/state")
+	public @ResponseBody List<Address> findByState(HttpServletRequest request,
+		   @RequestParam(value = "state") String state) throws ServiceUnavailableException {
+		return addressService.findByState(state);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/country")
+	public @ResponseBody List<Address> findByCountry(HttpServletRequest request,
+		   @RequestParam(value = "country") String country) throws ServiceUnavailableException {
+		return addressService.findByCountry(country);
+	}
+}
