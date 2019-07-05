02741cfbd5d57562fe263ed3ce563912cb8a83b0
shinewanna <bizleapintern2019@gmail.com>
2019-07-05 16:30:13 +0630

    JobResourceUpdate

---
+++ bizleap-hr/bizleap-service/src/main/java/com/bizleap/internhr/service/resource/AddressServiceResource.java
@@ -0,0 +1,20 @@
+package com.bizleap.internhr.service.resource;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.bizleap.commons.domain.entity.Address;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+
+public interface AddressServiceResource {
+	
+	boolean createAddress(HttpServletRequest request,@RequestBody Address address);
+	List<Address> getAllAddress(HttpServletRequest request) throws ServiceUnavailableException;
+	List<Address> findByAddressBoId(HttpServletRequest request,String boId) throws ServiceUnavailableException;
+	List<Address> findByCity(HttpServletRequest request, String city) throws ServiceUnavailableException;
+	List<Address> findByContactAddress(HttpServletRequest request, String contactAddress) throws ServiceUnavailableException;
+	List<Address> findByPermanentAddress(HttpServletRequest request, String permanentAddress) throws ServiceUnavailableException;
+	List<Address> findByState(HttpServletRequest request, String state) throws ServiceUnavailableException;
+	List<Address> findByCountry(HttpServletRequest request, String country) throws ServiceUnavailableException;
+}
---
+++ bizleap-hr/bizleap-service/src/main/java/com/bizleap/internhr/service/resource/impl/EmployeeServiceResourceImpl.java
@@ -0,0 +1,68 @@
+package com.bizleap.internhr.service.resource.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bizleap.commons.domain.entity.Employee;
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+import com.bizleap.internhr.service.resource.EmployeeServiceResource;
+import com.bizleap.service.EmployeeService;
+
+@RestController
+@RequestMapping(value = {"/employee"})
+public class EmployeeServiceResourceImpl implements EmployeeServiceResource {
+	
+	@Autowired 
+	EmployeeService employeeService;
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/new")
+	public @ResponseBody boolean createEmployee(HttpServletRequest request,@RequestBody Employee employee) {
+			try {
+				employeeService.saveEmployee(employee);
+			} catch (IOException e) {
+				return false;
+			} catch (ServiceUnavailableException e) {
+				return false;
+			}
+			return true;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/list")
+	public @ResponseBody List<Employee> getAllEmployee(HttpServletRequest request) throws ServiceUnavailableException {
+		return employeeService.getAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/{boId}")
+	public @ResponseBody List<Employee> findByBoId(HttpServletRequest request,
+		   @RequestParam(value = "boId") String boId) throws ServiceUnavailableException {
+		return employeeService.findByBoId(boId);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/firstName")
+	public @ResponseBody List<Employee> findByFirstName(HttpServletRequest request,
+		   @RequestParam(value = "firstName") String firstName) throws ServiceUnavailableException {
+		return employeeService.findByFirstName(firstName);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/lastName")
+	public @ResponseBody List<Employee> findByLastName(HttpServletRequest request,
+		   @RequestParam(value = "lastName") String lastName) throws ServiceUnavailableException {
+		return employeeService.findByLastName(lastName);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/find/gender")
+	public @ResponseBody List<Employee> findByGender(HttpServletRequest request,
+		   @RequestParam(value = "gender") String gender) throws ServiceUnavailableException {
+		return employeeService.findByGender(gender);
+	}
+}
